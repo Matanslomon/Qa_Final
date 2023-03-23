1ecf0e5119d772a4478a7d1d643310c2dfaa751e
Matan <matans727@gmail.com>
2023-03-23 21:28:35 +0200

    final

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
+driver = webdriver.Chrome()
+url = 'https://www.saucedemo.com/'
+driver.maximize_window()
+
+
 
 def open_site(driver, url):
     driver.get(url)
@@ -22,6 +27,32 @@ def signin(driver, username, password):
     button.click()
     time.sleep(3)
     return driver.current_url
+
+def count_match(driver):
+    add_button1 = driver.find_element(By.CSS_SELECTOR,'#add-to-cart-sauce-labs-backpack')
+    add_button1.click()
+    time.sleep(2)
+    add_button2 = driver.find_element(By.CSS_SELECTOR,'#add-to-cart-sauce-labs-bike-light')
+    add_button2.click()
+    time.sleep(2)
+    cart = driver.find_element(By.CSS_SELECTOR,'#shopping_cart_container > a')
+    cart.click()
+    time.sleep(2)
+    checkout = driver.find_element(By.CSS_SELECTOR,'#checkout')
+    checkout.click()
+    time.sleep(2)
+    firstname_box = driver.find_element(By.CSS_SELECTOR,'#first-name')
+    firstname_box.send_keys('mati')
+    lastname_box = driver.find_element(By.CSS_SELECTOR,'#last-name')
+    lastname_box.send_keys('sol')
+    zip_box = driver.find_element(By.CSS_SELECTOR,'#postal-code')
+    zip_box.send_keys('9664')
+    button_continue = driver.find_element(By.CSS_SELECTOR,'#continue')
+    button_continue.click()
+    time.sleep(2)
+    number=driver.find_element(By.CSS_SELECTOR,'#checkout_summary_container > div > div.summary_info > div.summary_info_label.summary_total_label')
+    number = number.text
+    return float(number[-5:])
 
 
 
--- test_qa.py
+++ test_qa.py
@@ -18,14 +18,22 @@ class TestSite:
         actual = open_site(driver,expected)
         assert expected == actual
 
-
-    @pytest.mark.parametrize('username,password',[ ('standard_user','secret_sauce'),('slomon','matan12') ])
+#-------------- sanity test ------------------------------------------------------------------#
+    @pytest.mark.parametrize('username,password',[ ('standard_user','secret_sauce')])
     def test_login(self,driver,url,username,password):
         expected = 'https://www.saucedemo.com/inventory.html'
         open_site(driver,url)
         actual = signin(driver, username , password )
         assert actual == expected
 
+#----------------------- login test ------------------------------------------------------#
+
+    def test_count(self,driver,url):
+        expected = 43.18
+        open_site(driver,url)
+        signin(driver,'standard_user','secret_sauce')
+        actual =count_match(driver)
+        assert actual == expected
 
 
 
