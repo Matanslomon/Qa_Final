e8879fc82372903a7856c1c8305305778b4cbd81
Matan <matans727@gmail.com>
2023-03-27 22:10:14 +0300

    final1

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import requests
 
-driver = webdriver.Chrome()
-url = 'https://www.saucedemo.com/'
-driver.maximize_window()
-
+# driver = webdriver.Chrome()
+# url = 'https://www.saucedemo.com/'
+# driver.maximize_window()
 
 
 def open_site(driver, url):
@@ -16,53 +16,29 @@ def open_site(driver, url):
     return driver.current_url
 
 
-
 def signin(driver, username, password):
-    name= driver.find_element(By.CSS_SELECTOR,'#user-name')
+    name = driver.find_element(By.CSS_SELECTOR, '#user-name')
     name.send_keys('standard_user')
-    password=driver.find_element(By.CSS_SELECTOR,'#password')
+    password = driver.find_element(By.CSS_SELECTOR, '#password')
     password.send_keys('secret_sauce')
     time.sleep(4)
-    button= driver.find_element(By.CSS_SELECTOR,'#login-button')
+    button = driver.find_element(By.CSS_SELECTOR, '#login-button')
     button.click()
     time.sleep(3)
     return driver.current_url
 
+
 def count_match(driver):
-    add_button1 = driver.find_element(By.CSS_SELECTOR,'#add-to-cart-sauce-labs-backpack')
+    add_button1 = driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-backpack')
     add_button1.click()
     time.sleep(2)
-    add_button2 = driver.find_element(By.CSS_SELECTOR,'#add-to-cart-sauce-labs-bike-light')
+    add_button2 = driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-bike-light')
     add_button2.click()
     time.sleep(2)
-    cart = driver.find_element(By.CSS_SELECTOR,'#shopping_cart_container > a')
+    cart = driver.find_element(By.CSS_SELECTOR, '#shopping_cart_container > a')
     cart.click()
     time.sleep(2)
-    checkout = driver.find_element(By.CSS_SELECTOR,'#checkout')
-    checkout.click()
-    time.sleep(2)
-    firstname_box = driver.find_element(By.CSS_SELECTOR,'#first-name')
-    firstname_box.send_keys('mati')
-    lastname_box = driver.find_element(By.CSS_SELECTOR,'#last-name')
-    lastname_box.send_keys('sol')
-    zip_box = driver.find_element(By.CSS_SELECTOR,'#postal-code')
-    zip_box.send_keys('9664')
-    button_continue = driver.find_element(By.CSS_SELECTOR,'#continue')
-    button_continue.click()
-    time.sleep(2)
-    number=driver.find_element(By.CSS_SELECTOR,'#checkout_summary_container > div > div.summary_info > div.summary_info_label.summary_total_label')
-    number = number.text
-    return float(number[-5:])
-
-
-def complete_test(driver):
-    add_Sauce_Labs_Onesie = driver.find_element(By.CSS_SELECTOR,'#add-to-cart-sauce-labs-onesie')
-    add_Sauce_Labs_Onesie.click()
-    time.sleep(2)
-    cart = driver.find_element(By.CSS_SELECTOR,'#shopping_cart_container > a')
-    cart.click()
-    time.sleep(2)
-    checkout = driver.find_element(By.CSS_SELECTOR,'#checkout')
+    checkout = driver.find_element(By.CSS_SELECTOR, '#checkout')
     checkout.click()
     time.sleep(2)
     firstname_box = driver.find_element(By.CSS_SELECTOR, '#first-name')
@@ -74,13 +50,77 @@ def complete_test(driver):
     button_continue = driver.find_element(By.CSS_SELECTOR, '#continue')
     button_continue.click()
     time.sleep(2)
-    finish = driver.find_element(By.CSS_SELECTOR,'#finish')
+    number = driver.find_element(By.CSS_SELECTOR,
+                                 '#checkout_summary_container > div > div.summary_info > div.summary_info_label.summary_total_label')
+    number = number.text
+    return float(number[-5:])
+
+
+def complete_test(driver):
+    add_Sauce_Labs_Onesie = driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-onesie')
+    add_Sauce_Labs_Onesie.click()
+    time.sleep(2)
+    cart = driver.find_element(By.CSS_SELECTOR, '#shopping_cart_container > a')
+    cart.click()
+    time.sleep(2)
+    checkout = driver.find_element(By.CSS_SELECTOR, '#checkout')
+    checkout.click()
+    time.sleep(2)
+    firstname_box = driver.find_element(By.CSS_SELECTOR, '#first-name')
+    firstname_box.send_keys('mati')
+    lastname_box = driver.find_element(By.CSS_SELECTOR, '#last-name')
+    lastname_box.send_keys('sol')
+    zip_box = driver.find_element(By.CSS_SELECTOR, '#postal-code')
+    zip_box.send_keys('9664')
+    button_continue = driver.find_element(By.CSS_SELECTOR, '#continue')
+    button_continue.click()
+    time.sleep(2)
+    finish = driver.find_element(By.CSS_SELECTOR, '#finish')
     finish.click()
     time.sleep(2)
     return driver.current_url
 
 
+def price_order(driver):
+    signin(driver, 'standard_user', 'secret_sauce')
+    time.sleep(2)
+    button = driver.find_element(By.CSS_SELECTOR, '#login-button')
+    button.click()
+    time.sleep(2)
+    name_button = driver.find_element(By.CSS_SELECTOR,
+                                      '#header_container > div.header_secondary_container > div > span > select')
+    name_button.click()
+    time.sleep(3)
+    price_button = driver.find_element(By.CSS_SELECTOR,
+                                       '#header_container > div.header_secondary_container > div > span > select')
+    price_button.click()
+    time.sleep(4)
+    return driver.current_url
 
 
+def send_http_request(url):
+    res = requests.get(url)
+    return res
+
+
+def check_status_code(code):
+    if 200 <= code < 400:
+        print(f"success -->  {code}")
+        return True
+    else:
+        print('error')
+        return False
+
+
+def check_word(url):
+    res = requests.get(url)
+    word = res.json()[0]
+
+    if 'dʒɔɪ' in word['phonetic']:
+        print(word['phonetic'])
+        return True
+    else:
+        print('error')
+        return False
 
 
--- test_qa.py
+++ test_qa.py
@@ -2,6 +2,7 @@ import pytest
 from main import *
 from selenium import webdriver
 
+
 class TestSite:
     @pytest.fixture
     def driver(self):
@@ -12,49 +13,62 @@ class TestSite:
     def url(self):
         return 'https://www.saucedemo.com/'
 
-
-    def test_sanity(self,driver,url):
+    def test_sanity(self, driver, url):
         expected = url
-        actual = open_site(driver,expected)
+        actual = open_site(driver, expected)
         assert expected == actual
 
-#-------------- sanity test ------------------------------------------------------------------#
-    @pytest.mark.parametrize('username,password',[ ('standard_user','secret_sauce')])
-    def test_login(self,driver,url,username,password):
+    # -------------- sanity test ------------------------------------------------------------------#
+
+    # ----------------- login test --------------------------------------------------#
+    @pytest.mark.parametrize('username,password', [('standard_user', 'secret_sauce')])
+    def test_login(self, driver, url, username, password):
         expected = 'https://www.saucedemo.com/inventory.html'
-        open_site(driver,url)
-        actual = signin(driver, username , password )
+        open_site(driver, url)
+        actual = signin(driver, username, password)
         assert actual == expected
 
-#----------------------- login test ------------------------------------------------------#
+    # ----------------------- login test ------------------------------------------------------#
 
-#-------------------------total price ---------------------------------------------------#
+    # -------------------------total price ---------------------------------------------------#
 
-    def test_count(self,driver,url):
+    def test_count(self, driver, url):
         expected = 43.18
-        open_site(driver,url)
-        signin(driver,'standard_user','secret_sauce')
-        actual =count_match(driver)
+        open_site(driver, url)
+        signin(driver, 'standard_user', 'secret_sauce')
+        actual = count_match(driver)
         assert actual == expected
 
-#-------------------------total price ---------------------------------------------------#
+    # -------------------------total price ---------------------------------------------------#
 
-#--------------------- complete test ----------------------------------------------------#
+    # --------------------- complete test ----------------------------------------------------#
 
-    def test_complete_purchase(self,driver,url):
+    def test_complete_purchase(self, driver, url):
         expected = 'https://www.saucedemo.com/checkout-complete.html'
-        open_site(driver,url)
-        signin(driver,'standard_user','secret_sauce')
+        open_site(driver, url)
+        signin(driver, 'standard_user', 'secret_sauce')
         actual = complete_test(driver)
         assert actual == expected
 
+    # --------------------- complete test --------------------------------------#
+
+    def test_products_order(self, driver, url):
+        expected = 'https://www.saucedemo.com/inventory.html'
+        open_site(driver, url)
+        signin(driver, 'standard_user', 'secret_sauce')
+        actual = price_order(driver)
+        assert actual == expected
 
 
+class TestApi:
+
+    def test_status_code(self):
+        url = 'https://api.dictionaryapi.dev/api/v2/entries/en/ignore'
+        res = send_http_request(url)
+        assert check_status_code(res.status_code) == True
 
 
-
-
-
-
-
-
+    def test_check_word(self):
+        url = 'https://api.dictionaryapi.dev/api/v2/entries/en/joy'
+        res = send_http_request(url)
+        assert check_word(url) == True
